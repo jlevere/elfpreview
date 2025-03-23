25e89b06e1feb9deb251e5d402d4fc66fdac48e9
jLevere <71566629+jLevere@users.noreply.github.com>
2025-03-23 01:59:13 -0400

    fixed eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,81 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 
-export default [{
-    files: ["**/*.ts"],
-}, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
+export default [
+    {
+        files: ["**/*.ts"],
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+        },
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: "module",
+                project: "./tsconfig.json",
+            },
+        },
+        rules: {
+            "@typescript-eslint/naming-convention": [
+                "warn",
+                {
+                    selector: "import",
+                    format: ["camelCase", "PascalCase"],
+                },
+                {
+                    selector: "variable",
+                    format: ["camelCase", "UPPER_CASE", "PascalCase"],
+                },
+                {
+                    selector: "function",
+                    format: ["camelCase", "PascalCase"],
+                },
+                {
+                    selector: "typeLike",
+                    format: ["PascalCase"],
+                },
+            ],
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "semi": "warn",
+
+            "curly": "warn",
+            "eqeqeq": "warn",
+            "no-throw-literal": "warn",
+            "semi": "off",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+        },
     },
 
-    languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+    {
+        files: ["**/elfpreview.ts"],
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/naming-convention": "off",
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+        },
     },
 
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+    {
+        files: ["**/extension/**/*.ts"],
+        rules: {
+            "@typescript-eslint/no-non-null-assertion": "off",
+        },
     },
-}];
+
+    {
+        ignores: ["**/*.rs", "**/*.wit"],
+    },
+
+    {
+        files: ["**/wasm/**/*.js"],
+        rules: {
+            "no-undef": "off",
+            "no-unused-vars": "off",
+        },
+    },
+];
