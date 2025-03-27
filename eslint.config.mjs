3f6634b6d4eb070f52cb3f0845ac79126e71d998
jLevere <71566629+jLevere@users.noreply.github.com>
2025-03-27 13:19:49 -0400

    moved to more modern eslint and added esbuild

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,81 +1,24 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default [
-    {
-        files: ["**/*.ts"],
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-        languageOptions: {
-            parser: tsParser,
-            parserOptions: {
-                ecmaVersion: 2022,
-                sourceType: "module",
-                project: "./ts/tsconfig.json",
-            },
-        },
-        rules: {
-            "@typescript-eslint/naming-convention": [
-                "warn",
-                {
-                    selector: "import",
-                    format: ["camelCase", "PascalCase"],
-                },
-                {
-                    selector: "variable",
-                    format: ["camelCase", "UPPER_CASE", "PascalCase"],
-                },
-                {
-                    selector: "function",
-                    format: ["camelCase", "PascalCase"],
-                },
-                {
-                    selector: "typeLike",
-                    format: ["PascalCase"],
-                },
-            ],
-            "@typescript-eslint/no-explicit-any": "warn",
-            "@typescript-eslint/explicit-function-return-type": "off",
-            "semi": "warn",
 
-            "curly": "warn",
-            "eqeqeq": "warn",
-            "no-throw-literal": "warn",
-            "semi": "off",
-            "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": "warn",
-        },
-    },
-
-    {
-        files: ["**/elfpreview.ts"],
-        rules: {
-            "@typescript-eslint/no-explicit-any": "off",
-            "@typescript-eslint/naming-convention": "off",
-            "no-unused-vars": "off",
-            "@typescript-eslint/no-unused-vars": "off",
-            "@typescript-eslint/no-non-null-assertion": "off",
-            "@typescript-eslint/ban-ts-comment": "off",
-        },
-    },
-
-    {
-        files: ["**/extension/**/*.ts"],
-        rules: {
-            "@typescript-eslint/no-non-null-assertion": "off",
-        },
-    },
-
-    {
-        ignores: ["**/*.rs", "**/*.wit"],
-    },
-
-    {
-        files: ["**/wasm/**/*.js"],
-        rules: {
-            "no-undef": "off",
-            "no-unused-vars": "off",
-        },
-    },
-];
+export default defineConfig([
+  {
+    ignores: [
+      "**/elfpreview.ts",
+    ]
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+    }
+  },
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  tseslint.configs.recommended,
+]);
