6fd851f288999fc8af6034e5bfe5a76948240e76
jlevere <71566629+jlevere@users.noreply.github.com>
2025-07-15 12:58:43 -0400

    Pkgs updates (#6)
    * updated flake and pnpm and fixed linter error from update
    
    * updated ignores
    
    * updated rust deps and added root cargo toml

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/d8b6e7045911cbe6b2d6a7e31c9c112e05b19221";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -22,6 +22,7 @@
         combine [
           stable.toolchain
           targets.wasm32-unknown-unknown.stable.rust-std
+          targets.wasm32-wasip1.stable.rust-std
         ];
     in {
       devShells = {
